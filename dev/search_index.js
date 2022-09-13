f8cf390e02ae858ffa1dfa69fc4bf167496f02a6
Documenter.jl <documenter@juliadocs.github.io>
2022-09-13 20:01:07 +0000

    build based on 148b396

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = BenEStock","category":"page"},{"location":"#BenEStock","page":"Home","title":"BenEStock","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for BenEStock.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [BenEStock]","category":"page"}]
+}
